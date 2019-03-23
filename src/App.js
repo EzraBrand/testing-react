35b6f63043d54e35d78b7718075b57ae74dfef93
Ezra Brand <ezrabrand@gmail.com>
2019-03-23 12:48:48 -0400

    messing around

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+ 
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src='https://i.ytimg.com/vi/Yj7ja6BANLM/maxresdefault.jpg' className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ezra <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
@@ -17,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn c
           </a>
         </header>
       </div>
